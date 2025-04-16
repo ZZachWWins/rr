c6b02d2c4b9c196dd4e7c2bdc055f27f6b1861ff
Zachary Winkle <zachthehippydesktop@Macmini.lan>
2025-04-16 15:52:37 -0400

    new changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,39 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./App.css";
 import Header from "./components/Header";
+import Contact from "./components/Contact";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
+  React.useEffect(() => {
+    gsap.utils.toArray(".section").forEach((section) => {
+      gsap.from(section, {
+        opacity: 0,
+        y: 100,
+        duration: 1,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: section,
+          start: "top 80%",
+        },
+      });
+    });
+  }, []);
+
   return (
     <Router>
       <div className="app">
         <Header />
-        <div style={{ padding: "20px" }}>Main Content Placeholder</div>
+        <Routes>
+          <Route
+            path="/contact"
+            element={<Contact />}
+          />
+        </Routes>
       </div>
     </Router>
   );
